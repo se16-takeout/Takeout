4f3a776ab151cb280cfa4f6017ff79a47dc498bf
zhoupenggang <2505113538@qq.com>
2019-06-17 14:05:26 +0800

    1

---
+++ app/src/main/java/cn/itcast/takeout/activity/GudieActivity.java
@@ -0,0 +1,16 @@
+package cn.itcast.takeout.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.itcast.takeout.R;
+
+public class GudieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gudie);
+    }
+}
---
+++ app/src/main/java/cn/itcast/takeout/activity/SplashActivity.java
@@ -0,0 +1,28 @@
+package cn.itcast.takeout.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import cn.itcast.takeout.R;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler mHandler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, GudieActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
