654aef8bec505af96a859442582fc4ee8b62b156
zhoupenggang <2505113538@qq.com>
2019-06-17 20:14:49 +0800

    logindemo

--- app/src/main/java/cn/itcast/takeout/activity/SplashActivity.java
+++ app/src/main/java/cn/itcast/takeout/activity/SplashActivity.java
@@ -19,7 +19,7 @@ public class SplashActivity extends AppCompatActivity {
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, GudieActivity.class);
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
---
+++ app/src/main/java/cn/itcast/takeout/utils/Contants.java
@@ -0,0 +1,9 @@
+package cn.itcast.takeout.utils;
+
+public class Contants {
+    public static final String USERNAME="13370714479";
+    public static final String PASSWORD="123456";
+
+    public static final String ENDPOINT="sssssss";
+    public static final String ID="zpg";
+}
