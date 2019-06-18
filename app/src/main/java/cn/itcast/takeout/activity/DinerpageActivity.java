1c43e2190efd542069aadf7387deaa200c0ce9b7
zhoupenggang <2505113538@qq.com>
2019-06-18 12:25:31 +0800

    *

---
+++ app/src/main/java/cn/itcast/takeout/activity/DinerpageActivity.java
@@ -0,0 +1,16 @@
+package cn.itcast.takeout.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.itcast.takeout.R;
+
+public class DinerpageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dinerpage);
+    }
+}
--- app/src/main/java/cn/itcast/takeout/utils/Contants.java
+++ app/src/main/java/cn/itcast/takeout/utils/Contants.java
@@ -1,7 +1,7 @@
 package cn.itcast.takeout.utils;
 
 public class Contants {
-    public static final String USERNAME="13370714479";
+    public static final String USERNAME="123456";
     public static final String PASSWORD="123456";
 
     public static final String ENDPOINT="sssssss";
